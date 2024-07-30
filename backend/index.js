2aa2a5c13ae3fc1a8b4682a225a8a0129f816031
Muhammad Anees <134684670+manees101@users.noreply.github.com>
2024-07-30 15:52:10 +0500

    refactor: server url on frontend

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }))
 app.use(cors({
-  origin:[process.env.CLIENT_URL],
+  origin:["https://store-zen-frontend.vercel.app"],
   credentials:true
 }))
 app.use(morgan("tiny"))
